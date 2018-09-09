fda84bd57cacf67abd5506779c04beaba04dba44
jackzhujie <15972806478@163.com>
2018-09-09 16:30:40 +0800

    添加基础教程

--- src/page/vueBase.vue
+++ src/page/vueBase.vue
@@ -3,8 +3,40 @@
     <div class="vueBase">
       <ul>
         <li>
-          <div class="title">1.基础语法</div>
+          <div class="title">vue是什么</div>
 
+        </li>
+        <li>
+          <div class="title">环境搭建</div>
+
+        </li>
+        <li>
+          <div class="title">基础语法</div>
+          <span>{{}},v-bind,v-on</span>
+        </li>
+        <li>
+          <div class="title">内置指令</div>
+          <span>v-text,v-if,v-show,v-html,v-bind,v-model,v-for,</span>
+        </li>
+        <li>
+          <div class="title">style,class</div>
+          <span>v-text,v-if,v-show,v-html,v-bind,v-model,v-for,</span>
+        </li>
+        <li>
+          <div class="title">style,class</div>
+          <span>v-text,v-if,v-show,v-html,v-bind,v-model,v-for,</span>
+        </li>
+        <li>
+          <div class="title">v-model</div>
+          <span>v-text,v-if,v-show,v-html,v-bind,v-model,v-for,</span>
+        </li>
+        <li>
+          <div class="title">组件</div>
+          <span>v-text,v-if,v-show,v-html,v-bind,v-model,v-for,</span>
+        </li>
+        <li>
+          <div class="title">computed,watch</div>
+          <span>v-text,v-if,v-show,v-html,v-bind,v-model,v-for,</span>
         </li>
       </ul>
     </div>
