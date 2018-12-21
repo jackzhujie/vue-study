063ce6fb76cfdc2f6375401591f2a9aa65c82af6
朱捷 <630796280@qq.com>
2018-12-21 15:25:59 +0800

    调整代码结构

---
+++ src/page/layout.vue
@@ -0,0 +1,167 @@
+<template>
+    <!--布局路由，所有页面都嵌入这个路由页面-->
+    <div class="layout">
+        <el-row :gutter="20">
+            <el-col :span="4">
+                <!--菜单-->
+                <el-menu
+                        :default-active="activeIndex"
+                        @select="handleSelect"
+                        class="el-menu-vertical-demo">
+                    <el-submenu :index="index + ''" v-for="(item,index) in menuList" :key="index">
+                        <template slot="title">   <!--组件插值，slot意思是把下面的html插到组件里面对应的地方-->
+                            <i :class="item.css"></i>
+                            <span v-text="item.showName"></span>
+                        </template>
+                        <el-menu-item-group>
+                            <el-menu-item :index="index + '-' + subIndex" v-for="(subItem,subIndex) in item.child"
+                                          :key="subIndex">
+                                <router-link :to="{path:subItem.url}" v-text="subItem.showName"></router-link>
+                            </el-menu-item>
+                        </el-menu-item-group>
+                    </el-submenu>
+                </el-menu>
+            </el-col>
+            <el-col :span="20">
+                <router-view></router-view>    <!--单页面应用，所有的内容在这个节点切换-->
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+    import test1 from '@/page/testPage1.vue'
+    import test2 from '@/page/testPage2.vue'
+    import vueBase from '@/page/vueBase.vue'
+    import componentStudy from '@/page/componentStudy.vue'
+    import vuedirective from '@/page/vueDirective.vue'
+    import pageTest from '@/page/pageComponentsTest.vue'
+    import mapToArrayTest from '@/page/mapToArrayTest.vue'
+    import selectRepeat from '@/page/selectRepeat.vue'
+    import serverTest from '@/page/serverTest.vue'
+    import recursiveList from "@/page/recursiveList.vue"
+    export default {
+        name: "layout",
+        data() {
+            return {
+                menuList: [],
+                routeData: [
+                    {
+                        path: '/',
+                        name: 'test1',
+                        component: test1
+                    },
+                    {
+                        path: '/test2',
+                        name: 'test2',
+                        component: test2
+                    },
+                    {
+                        path: '/baseStudy',
+                        name: '基础学习',
+                        component: vueBase
+                    },
+                    {
+                        path: '/componentStudy',
+                        name: '组件教程',
+                        component: componentStudy
+                    },
+                    {
+                        path: '/vueDirective',
+                        name: '自定义指令教程',
+                        component: vuedirective
+                    },
+                    {
+                        path: '/pageTest',
+                        name: '分页组件测试',
+                        component: pageTest
+                    },
+                    {
+                        path: '/mapToArrayTest',
+                        name: 'map代替array避免遍历',
+                        component: mapToArrayTest
+                    },
+                    {
+                        path: '/selectRepeat',
+                        component: selectRepeat
+                    },
+                    {
+                        path: '/serverTest',
+                        component: serverTest
+                    },
+                    {
+                        path:"/recursiveList",
+                        component:recursiveList
+                    }
+                ]
+            }
+        },
+        methods: {
+            handleSelect: function (key, keyPath) {
+                console.log(key, keyPath);
+                this.$store.commit('setMenuPosition', keyPath[1])     //这里存在菜单位置
+            },
+            dealMenu() {
+                //利用vue-router2.2.0新增特性addRoutes实现路由动态加载，菜单动态加载，运用于后台管理系统，路由数据取自数据库
+                let arr = []
+                this.menuList.forEach(item => {
+                    item.child.forEach(row => {
+                        //根据后台返回的url，这里加载对于的route
+                        this.routeData.forEach(route => {
+                            if (row.url == route.path) {
+                                arr.push(route)
+                            }
+                        })
+                    })
+                })
+                const baseRoute = [{
+                    path: '/',
+                    name: '模板',
+                    children:[]
+                }]
+                baseRoute[0].children = arr
+                this.$router.addRoutes(baseRoute)
+            },
+            getMenuRoute: function () {
+                const defaultMenu = [
+                    {
+                        showName: "首页", css: "el-icon-star-off", child: [{showName: "测试1", url: "/"}]
+                    }, {
+                        showName: "node服务测试",
+                        css: "el-icon-circle-check-outline",
+                        child: [{showName: "增删改查", url: "/serverTest"}]
+                    }, {
+                        showName: "vue学习教程", css: "el-icon-edit-outline", child: [
+                            {showName: "基础教程", url: "/baseStudy"},
+                            {showName: "组件教程", url: "/componentStudy"},
+                            {showName: "自定义指令教程", url: "/vueDirective"},
+                            {showName: "分页组件", url: '/pageTest'}
+                        ]
+                    },
+                    {
+                        showName: 'js技巧', css: 'el-icon-circle-check-outline', child: [
+                            {showName: 'map代替array避免遍历', url: '/mapToArrayTest'},
+                            {showName: '多个下拉去重', url: '/selectRepeat'},
+                            {showName:"递归遍历",url:'/recursiveList'}
+                        ]
+                    }
+                ]
+                this.menuList = defaultMenu
+                this.dealMenu()
+            }
+        },
+        computed: {
+            activeIndex: function () {              //从vuex获取数据
+                return this.$store.state.menuPosition[0]
+            }
+        },
+        created: function () {
+            this.getMenuRoute()
+        }
+    }
+</script>
+
+<style lang="scss">
+    .layout {
+    }
+</style>
--- src/router.js
+++ src/router.js
@@ -1,14 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import login from './page/login'
+import layout from './page/layout'
 
 Vue.use(Router)
 
 export default new Router({
-  // mode: 'history',     //这个模式下，地址栏就不会自己加上#号了
-  routes: [{
-    name:'login',
-    path:'/login',
-    component: login
-  }]
+    // mode: 'history',     //这个模式下，地址栏就不会自己加上#号了
+    routes: [
+        {
+            name: 'login',
+            path: '/login',
+            component: login
+        },
+        {
+            name: 'layout',
+            path:'/',
+            component:layout
+        }
+    ]
 })
