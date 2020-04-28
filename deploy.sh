d2cd25fd295a66108655ffd3578b329fbe7088e4
zhujie <630796280@qq.com>
2020-04-28 14:13:16 +0800

    添加打包命令

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+# deploy.sh
+# 错误时停止
+set -e
+
+# 打包
+npm run build
+# 切换分支
+git checkout -b gh-pages
+# 添加dist目录
+git add -f dist
+git commit -m 'deploy'
+
+# 提交到 gh-pages 分支
+git subtree push --prefix dist origin gh-pages
+
+cd -
