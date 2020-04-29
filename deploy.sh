ee28151c56df316b113bd8bba5dc8ae0acc76694
zhujie <630796280@qq.com>
2020-04-29 11:08:10 +0800

    修改发布文件

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,17 @@
 # deploy.sh
 # 错误时停止
-set -e
+# set -e
 
 # 打包
 npm run build
 # 切换分支
-git checkout -b gh-pages
+git checkout gh-pages
 # 添加dist目录
 git add -f dist
 git commit -m 'deploy'
 
 # 提交到 gh-pages 分支
 git subtree push --prefix dist origin gh-pages
+git checkout master
 
 cd -
