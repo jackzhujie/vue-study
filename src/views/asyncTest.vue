5056a481d05bd8f278ca95c9e5853cfe3ad2a5a2
zhujie <630796280@qq.com>
2020-04-29 10:58:42 +0800

    添加异步测试代码

---
+++ src/views/asyncTest.vue
@@ -0,0 +1,42 @@
+<template>
+    <!--    异步测试-->
+    <div class="asyncTest">
+        <h1>2020年4月29日问题：前端异步是什么？哪些情况下会发生异步？</h1>
+        <h5>可以打开控制台查看运行结果</h5>
+    </div>
+</template>
+
+<script>
+	const moment = require('moment')
+	export default {
+		name: "asyncTest",
+		components: {},
+		data() {
+			return {}
+		},
+		methods: {
+			asyncFun() {
+				return new Promise((resolve) => {
+					console.log('1', '时间：', moment().format('YYYY-MM-DD hh:mm:ss'))
+					setTimeout(() => {
+						console.log('2', '时间：', moment().format('YYYY-MM-DD hh:mm:ss'))
+						resolve()
+					}, 1000)
+					console.log('3', '时间：', moment().format('YYYY-MM-DD hh:mm:ss'))
+				})
+			},
+			async run() {
+				await this.asyncFun()
+			}
+		},
+		computed: {},
+		created: function () {
+			this.run()
+		}
+	}
+</script>
+
+<style lang="scss">
+    .asyncTest {
+    }
+</style>
